ef8336d07fda762d2dad877eb611abc8c9a5b5fd
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-02-13 18:57:45 +0000

    Add welcome introduction and Ask player for name

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_name():
+    """
+    Welcome message and get name from user
+    """
+    print("*********************")
+    print("       Hangman")
+    print("Guess the word to win!")
+    print("*********************\n")
+
+    while 
