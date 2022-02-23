6bbc2407823e4d925473ede60c0fe27ec40e528b
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-02-23 20:36:06 +0000

    Add Game Over message and add code to display chosen word

--- run.py
+++ run.py
@@ -128,7 +128,7 @@ def start_game(word):
     # If no turns left shows right answer
     while attempts > 0 and len(letters) > 0:
         show_answer = [
-            letters if letters in guesses else "_" for letters in word
+            letters if letters in guesses else "_" for letters in word 
         ]
         # print(hangman_status(attempts))
         print("\n")
@@ -157,6 +157,14 @@ def start_game(word):
                 letters.remove(players_turn)
             else:
                 print("\033[0;31mPlease make a valid choice.")
+        
+        if attempts == 0:
+            #No more attempts left.
+            print("\033[0mAttempts Remaining: ", attempts)
+            guesses.append(players_turn)
+            print("\033[0;31mAwh you lose!")
+            print("The correct word was", word, "\n")
+            
     #Ask if they want to play again
     print("\033[0;36mWould you like to play again?")
     play_again()
